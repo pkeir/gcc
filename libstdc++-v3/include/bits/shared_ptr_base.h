70bf0a38afdbf38383b6f603bb55173ffaf74acf
Paul Keir <paul.keir@uws.ac.uk>
2025-05-23 14:28:19 +0100

    Added owner_equal member functions.

--- libstdc++-v3/include/bits/shared_ptr_base.h
+++ libstdc++-v3/include/bits/shared_ptr_base.h
@@ -1122,6 +1122,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       _M_less(const __weak_count<_Lp>& __rhs) const noexcept
       { return std::less<_Sp_counted_base<_Lp>*>()(this->_M_pi, __rhs._M_pi); }
 
+      size_t
+      _M_owner_hash() const noexcept
+      { return std::hash<_Sp_counted_base<_Lp>*>()(this->_M_pi); }
+
       // Friend function injected into enclosing namespace and found by ADL
       friend inline bool
       operator==(const __shared_count& __a, const __shared_count& __b) noexcept
@@ -1224,6 +1228,10 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       bool
       _M_less(const __shared_count<_Lp>& __rhs) const noexcept
       { return std::less<_Sp_counted_base<_Lp>*>()(this->_M_pi, __rhs._M_pi); }
+
+      size_t
+      _M_owner_hash() const noexcept
+      { return std::hash<_Sp_counted_base<_Lp>*>()(this->_M_pi); }
 
       // Friend function injected into enclosing namespace and found by ADL
       friend inline bool
@@ -1715,6 +1723,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	{ return _M_refcount._M_less(__rhs._M_refcount); }
       /// @}
 
+      size_t owner_hash() const noexcept { return _M_refcount._M_owner_hash(); }
+
       template<typename _Tp1>
       bool
       owner_equal(__shared_ptr<_Tp1, _Lp> const& __rhs) const noexcept
@@ -2107,6 +2117,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	bool
 	owner_before(const __weak_ptr<_Tp1, _Lp>& __rhs) const noexcept
 	{ return _M_refcount._M_less(__rhs._M_refcount); }
+
+      size_t owner_hash() const noexcept { return _M_refcount._M_owner_hash(); }
 
       template<typename _Tp1>
       bool
